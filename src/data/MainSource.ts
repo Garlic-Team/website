4b90d2eeea57ab1859fd887b6c7002213a50a471
Hyro <generalkubo@gmail.com>
2021-12-11 22:56:00 +0100

    Update MainSource.ts

--- src/data/MainSource.ts
+++ src/data/MainSource.ts
@@ -9,7 +9,7 @@ export default new DocsSource({
 	global: 'GCommands',
 	repo: 'Garlic-Team/GCommands',
 	docsFolder: '/docs',
-	defaultTag: 'main',
+	defaultTag: 'next',
 	branchFilter: (branch: string) => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
 	tagFilter: (tag: string) => semver.gte(tag.replace(/^v/, ''), '9.0.0'),
 });
