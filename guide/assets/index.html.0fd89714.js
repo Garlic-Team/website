b2bca400591d1062fe9c4c078d68e9df41510ba7
S222em <68066476+S222em@users.noreply.github.com>
2022-01-03 18:11:05 +0000

    Deploying to gh-pages from @ Garlic-Team/website@eed40cbcd1344907cf7907ddf884093c4c43f7f4 🚀

---
+++ guide/assets/index.html.0fd89714.js
@@ -0,0 +1,1 @@
+const e={key:"v-0276283d",path:"/hyttpo/getting-started/",title:"Getting started",lang:"en-US",frontmatter:{},excerpt:"",headers:[{level:2,title:"Installing dependencies",slug:"installing-dependencies",children:[]},{level:2,title:"Basic setup",slug:"basic-setup",children:[]}],filePathRelative:"hyttpo/getting-started/README.md",git:{updatedTime:1641233386e3}};export{e as data};
