4987d47c1099dc981e3225cb2d50805f50755893
xhyrom <generalkubo@gmail.com>
2022-01-25 21:32:01 +0100

    fix: mainsource

--- src/data/MainSource.ts
+++ src/data/MainSource.ts
@@ -2,14 +2,15 @@ import semver from 'semver';
 
 import DocsSource from './DocsSource';
 
-const branchBlacklist = new Set(['docs', 'webpack', 'v8', 'master', 'renovate']);
+const branchBlacklistArray = ['renovate', 'v8', 'gh-pages', 'docs', 'v8-dev']
+const branchBlacklist = new Set(['docs', 'webpack', 'v8', 'v8-dev', 'master', 'renovate', 'gh-pages']);
 export default new DocsSource({
 	id: 'gcommands',
 	name: 'GCommands',
 	global: 'GCommands',
 	repo: 'Garlic-Team/GCommands',
 	docsFolder: '/docs',
-	defaultTag: 'next',
-	branchFilter: (branch: string) => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
+	defaultTag: 'latest',
+	branchFilter: (branch: string) => (!branchBlacklistArray.includes(branch) || !branchBlacklist.has(branch)) && !branch.startsWith('dependabot/'),
 	tagFilter: (tag: string) => semver.gte(tag.replace(/^v/, ''), '9.0.0'),
 });
