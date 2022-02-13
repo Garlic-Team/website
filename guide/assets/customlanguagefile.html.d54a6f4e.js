785430d6acafd18cd57ccaeec77b3d1722b8209b
xHyroM <generalkubo@gmail.com>
2022-02-13 11:39:54 +0000

    Deploying to gh-pages from @ Garlic-Team/website@447183c61362dfe2901bf8b61fed3f16a6266d07 🚀

--- guide/assets/customlanguagefile.html.d54a6f4e.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-5f9ee1c6",path:"/gcommands-legacy/other/customlanguagefile.html",title:"Using a custom language file",lang:"en-US",frontmatter:{},excerpt:"",headers:[],filePathRelative:"gcommands-legacy/other/customlanguagefile.md",git:{updatedTime:1644605418e3}};export{e as data};
